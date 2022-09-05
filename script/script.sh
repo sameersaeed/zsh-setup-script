b00ffdf9254c75d68f84c022d245cb4e2af44756
sameersaeed <70821949+sameersaeed@users.noreply.github.com>
2022-09-05 14:16:16 -0400

    Update script.sh

--- script/script.sh
+++ script/script.sh
@@ -1,8 +1,10 @@
+#arguments that will be taken when script is run (i.e "./script args1 args2 args3 args4")
 plgn=$1
 thm=$2
 exp=$3
 src=$4
 
+#finds values and inserts, given arguments
 sed -i -e "s/\(plugins=\).*/\1$1/" \
 -e "s/\(ZSH_THEME=\).*/\1$2/" \
 -e "s/\(export \).*/\1$3/" \
